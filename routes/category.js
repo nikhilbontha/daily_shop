67705e167ca78e104619dc3af4593aa39ed8370d
nikhilbontha <nikhilbontha00@gmail.com>
2025-10-12 10:13:47 +0530

    Describe what you changed

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/product');
+const Product = require('../models/Product');
 
 // Show all categories
 router.get('/', (req, res) => {
--- routes/checkout.js
+++ routes/checkout.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Product = require('../models/product');
+const Product = require('../models/Product');
 
 router.get('/checkout', async (req, res) => {
   if (!req.session || !req.session.cart || req.session.cart.length === 0) {
